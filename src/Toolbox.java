55c6cb7c125a3bb43e5ba0ce0364a0e75e958b6c
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-02-08 02:45:36 -0800

    removeGiants

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -170,11 +170,12 @@ public class Toolbox {
       throw new IllegalArgumentException("Head cannot be null.");
     }
     SingleNode current = head;
-    while(current.next != null) {
-      if(current.next.data > current.data) {
-        current.next = current.next.next;
-      }
-      current = current.next;
+    while (current.next != null && current.next.next != null) {
+        if (current.next.data > current.next.next.data) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
     }
     
   }
